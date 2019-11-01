bec500b0ea8e3cd69fcc24d922217dfbd4a5e9a5
minecire <unconfigured@null.spigotmc.org>
2019-11-01 18:13:21 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var ctx = document.getElementById("game").getContext("2d"); //get the context of the canvas
+function runFrame(){
+    
+}
+
+var interval = setInterval(runFrame,1000/60); // run the function 'runFrame' every 60th of a second
