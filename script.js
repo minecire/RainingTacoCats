eac8b6b46c03e7914e03e097f14238de04b89ddb
minecire <unconfigured@null.spigotmc.org>
2019-11-04 06:23:39 -0500

    workin on rain

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var ctx = canvas.getContext("2d"); //get the context of the canvas
 var frame = 0;
 var prevFrame = 0;
 var cats = [];
+var raindrops = [];
 function runFrame(){
+    for(var i = 0; i < Math.random()*5+5; i++){
+        raindrops.push(new raindrop(Math.random()*canvas.width,Math.random()*40+10 , Math.round(Math.random()*10)));
+    }
     if((Math.random()*100 > 30 && frame-prevFrame > 2) || frame-prevFrame > 6){
         cats.push(new cat(Math.random()*canvas.width,Math.random()*40+10 , Math.round(Math.random()*10)));
         prevFrame = frame;
@@ -19,22 +23,60 @@ function runFrame(){
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for(var i = 0; i < cats.length; i++){
         cats[i].displayCat();
-        cats[i].update();
+        if(cats[i].updateCat()){
+            var cats2 = [];
+            for(var j = 0; j < cats.length; j++){
+                if(j != i){
+                    cats2.push(cats[j]);
+                }
+            }
+            cats = cats2;
+            i--;
+        }
+    }
+    for(var i = 0; i < raindrops.length; i++){
+        raindrops[i].display();
+        if(raindrops[i].update()){
+            var raindrops2 = [];
+            for(var j = 0; j < raindrops.length; j++){
+                if(j != i){
+                    raindrops2.push(raindrops[j]);
+                }
+            }
+            raindrops = raindrops2;
+            i--;
+        }
     }
     frame++;
 }
 
 var interval = setInterval(runFrame,1000/60); // run the function 'runFrame' every 60th of a second
 
-class cat{
-    constructor(xPos, cLength, animOffset){
+class raindrop{
+    constructor(xPos, cLength, animOffset) {
         this.animationOffset = animOffset;
         this.x = xPos;
         this.cycleLength = cLength;
         this.y = 0;
         this.yv = 3;
         this.state = "falling";
+        this.height = Math.random(50,150);
     }
+    update(){
+        this.y += this.yv;
+        this.yv += 1;
+        if(this.y > canvas.height+300){
+            return true;
+        }
+    }
+    display(){
+        ctx.fillStyle = "#556677";
+        ctx.fillRect(this.x, this.y, 2, this.height);
+    }
+}
+
+class cat extends raindrop{
+    
     displayCat(){
         ctx.translate(this.x, this.y);
         ctx.rotate((frame + this.animationOffset) / this.cycleLength);
@@ -48,8 +90,11 @@ class cat{
         ctx.rotate(-(frame + this.animationOffset) / this.cycleLength);
         ctx.translate(-this.x, -this.y);
     }
-    update(){
+    updateCat(){
         this.y += this.yv;
         this.yv += 0.3;
+        if(this.y > canvas.height+300){
+            return true;
+        }
     }
 }
